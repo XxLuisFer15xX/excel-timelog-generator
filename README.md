cf7de8d9389605aedb184c3026509f3778c02812
Luis Solano <lsolano@goconsultores.com>
2023-09-11 12:41:01 -0600

    :recycle: Updating the project description and preparing a version of the application.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Base-React-TS
+# Excel-Timelog-Generator
 
 ## Steps to follow
 1. Clone the project.
@@ -8,8 +8,7 @@
 5. Install the "eslint" plugin.
 6. Choose prettier as the primary formatter for .js, .jsx, .ts, and .tsx files.
 7. Activate automatic formatting when saving Visual Studio Code.
-8. Add `.env` file.
-9. Run `npm i` to install dependencies.
-10. Run `npm run dev` to run the project in development mode.
-11. Run `npm run build` to compile the project.
-12. Run `npm start` to run the project in production mode.
+8. Run `npm i` to install dependencies.
+9. Run `npm run dev` to run the project in development mode.
+10. Run `npm run build` to compile the project.
+11. Run `npm start` to run the project in production mode.
--- src/components/routes/AppRouter.tsx
+++ src/components/routes/AppRouter.tsx
@@ -6,19 +6,7 @@ import {
 } from 'react-router-dom'
 
 // Components
-import {
-  ComponentsAlert,
-  ComponentsButton,
-  ComponentsDialog,
-  ComponentsInputs1,
-  ComponentsInputs2,
-  ComponentsInputs3,
-  ComponentsInputs4,
-  ComponentsLoader,
-  ComponentsTable,
-  ComponentsText,
-  Home,
-} from '@pages'
+import { Home } from '@pages'
 import { Error404 } from '@pages'
 
 export const AppRouter = () => {
@@ -26,16 +14,6 @@ export const AppRouter = () => {
     <Router>
       <Routes>
         <Route path="/home" element={<Home />} />
-        <Route path="/componentsAlert" element={<ComponentsAlert />} />
-        <Route path="/componentsButton" element={<ComponentsButton />} />
-        <Route path="/componentsDialog" element={<ComponentsDialog />} />
-        <Route path="/componentsInputs1" element={<ComponentsInputs1 />} />
-        <Route path="/componentsInputs2" element={<ComponentsInputs2 />} />
-        <Route path="/componentsInputs3" element={<ComponentsInputs3 />} />
-        <Route path="/componentsInputs4" element={<ComponentsInputs4 />} />
-        <Route path="/componentsLoader" element={<ComponentsLoader />} />
-        <Route path="/componentsTable" element={<ComponentsTable />} />
-        <Route path="/componentsText" element={<ComponentsText />} />
         <Route path="/page-error" element={<Error404 />} />
         <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="*" element={<Navigate to="/page-error" replace />} />
